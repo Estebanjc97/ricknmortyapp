44e52d5f3fb852ce06e1eb90c7c4c31b86100bce
Esteban <44655087+Estebanjc97@users.noreply.github.com>
2024-12-18 18:18:07 -0500

    Dev (#8)
    * server, config service
    
    * Basic API interaction
    
    * Characters List
    
    * Pagination
    
    * Ui improvements
    
    * View, edit and create character logic
    
    * details
    
    * prod endpoint
    
    * Firebase auth client
    
    * server middleware
    
    ---------
    
    Co-authored-by: EstebanUnity <esteban.jimenez@unity3d.com>

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,10 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, inject, PLATFORM_ID, provideZoneChangeDetection, REQUEST } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideFirebaseApp, initializeApp, initializeServerApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { isPlatformBrowser } from '@angular/common';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAwL6i1t3wxlrl4AcVbVPmUiqU6MNgjjRI",
+  authDomain: "ricknmortyapp-444617.firebaseapp.com",
+  projectId: "ricknmortyapp-444617",
+  storageBucket: "ricknmortyapp-444617.firebasestorage.app",
+  messagingSenderId: "467042232272",
+  appId: "1:467042232272:web:73485e02d816fa540a7bd5"
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,6 +23,18 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes), 
     provideClientHydration(withEventReplay()), 
     provideAnimationsAsync(),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    provideFirebaseApp(() => {
+      if (isPlatformBrowser(inject(PLATFORM_ID))) {
+        return initializeApp(firebaseConfig);
+      }
+      const request = inject(REQUEST, { optional: true });
+      const authIdToken = request?.headers.get("Authorization")?.split("Bearer ")[1];
+      return initializeServerApp(firebaseConfig, {
+        authIdToken,
+        releaseOnDeref: request || undefined
+      });
+    }),
+    provideAuth(() => getAuth()),
   ]
 };
--- client/src/app/components/layout/navbar/navbar.component.ts
+++ client/src/app/components/layout/navbar/navbar.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatTooltipModule} from '@angular/material/tooltip';
+import { Component, inject, OnDestroy, signal } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { Auth, GoogleAuthProvider, signInWithPopup, User, user } from '@angular/fire/auth';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -10,6 +12,29 @@ import {MatTooltipModule} from '@angular/material/tooltip';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnDestroy {
+  private auth: Auth = inject(Auth);
+  user = user(this.auth);
+  userSubscription: Subscription;
+  currentUser = signal<User | null>(null);
+
+  constructor() {
+    this.userSubscription = this.user.subscribe((aUser: User | null) => {
+      this.currentUser.set(aUser);
+    })
+  }
+
+  async login() {
+    await signInWithPopup(this.auth, new GoogleAuthProvider);
+  }
+
+  async logout() {
+    await this.auth.signOut();
+    alert("Logged out");
+  }
+
+  ngOnDestroy() {
+    this.userSubscription.unsubscribe();
+  }
 
 }
